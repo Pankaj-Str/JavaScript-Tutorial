1910b90bb94c100fd1edce5be9899b94f0950e40
Pankaj Chouhan <pankajchouhan80@gmail.com>
2024-04-25 08:42:05 +0530

    Add files via upload

---
+++ Example/Basic Type Conversion/TypeConversion.js
@@ -0,0 +1,28 @@
+// parseInt()
+// parseFloat()
+// String()
+// typeof()
+
+let number = '90';
+console.log(typeof(number))
+
+let data = 80;
+console.log(typeof(data))
+
+// Type Conversion
+
+data1 = parseInt(number);
+
+result = data1 + data;
+console.log("this is result : "+result);
+console.log(typeof(data1));
+
+num = '4.5';
+
+floatResult = parseFloat(num);
+
+console.log(floatResult);
+console.log(typeof(floatResult));
+
+
+
