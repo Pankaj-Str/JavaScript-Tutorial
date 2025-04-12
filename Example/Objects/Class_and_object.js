98b1446a2820c18d8b467370ba767468da9c2eb0
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-04-12 07:17:49 +0000

    Add Class_and_object.js with Employee class implementation and method examples

---
+++ Example/Objects/Class_and_object.js
@@ -0,0 +1,21 @@
+// create a Employee Class
+
+class Employee{
+
+    info() {
+        console.log("Employee Name : Rohan")
+    }
+
+
+    Salary(Emp_Salary){
+        console.log("This is Salary : ",Emp_Salary)
+    }
+
+}
+
+// function calling 
+// create a object 
+emp = new Employee();
+
+emp.info();
+emp.Salary(4500);
