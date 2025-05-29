8e614922dce06e235665ad9a697ae27563a941dd
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-05-29 14:32:06 +0530

    Add JavaScript Objects example and corresponding HTML file

---
+++ Example/01 Lecture_Example/Basic/JavaScript Objects.js
@@ -0,0 +1,26 @@
+// object 
+
+const Days = {
+    Day1:"Monday",
+    Day2:"Tuesday"
+}
+
+
+console.log("Days ",Days);
+console.log("Days ",Days.Day1);
+
+
+class info{
+
+    Print_Data() {
+
+        console.log("This is Demo ... Data ")
+
+    }
+
+
+}
+
+obj = new info
+
+obj.Print_Data();
