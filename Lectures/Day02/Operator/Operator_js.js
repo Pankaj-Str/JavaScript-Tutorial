cfeb94f4239ee72a4d4ad7f6c81429aaaf272f9f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-10-25 11:05:28 +0530

    Add Operator.html and Operator_js.js with arithmetic, comparison, and logical operators examples; Add Variables.html demonstrating variable declarations and usage

---
+++ Lectures/Day02/Operator/Operator_js.js
@@ -0,0 +1,48 @@
+// Arithmetic Operators:
+// + (Addition)
+// - (Subtraction)
+// * (Multiplication)
+// / (Division)
+// % (Modulus, returns the remainder)
+// ++ (Increment by 1)
+// -- (Decrement by 1)
+x = 345
+y = 67
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(++x);
+console.log(--y);
+
+// Comparison Operators:
+
+// == (Equal to)
+// != (Not equal to)
+// === (Strict equal to)
+// !== (Strict not equal to)
+// > (Greater than)
+// < (Less than)
+// >= (Greater than or equal to)
+// <= (Less than or equal to)
+x = 34
+y = 67
+console.log(x==y)
+console.log(x!=y)
+console.log(x>y)
+console.log(x<y)
+console.log(x<=34)
+console.log(x>=34)
+
+console.log(34 !== '34')
+
+// Logical Operators:
+
+// && (Logical AND)
+console.log((x<y) && (x==304));
+// || (Logical OR)
+console.log((x<y) || (x==340));
+// ! (Logical NOT)
+console.log(!(x<y));
